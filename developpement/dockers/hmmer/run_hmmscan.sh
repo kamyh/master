b28a6dbf084238f092ad46e96805e42eb167cdeb
Deruaz Vincent <deruazvincent@gmail.com>
2017-02-06 12:23:12 +0100

    repport

--- developpement/dockers/hmmer/run_hmmscan.sh
+++ developpement/dockers/hmmer/run_hmmscan.sh
@@ -3,4 +3,4 @@
 # hmmpress /data/Pfam-A.hmm only first time fter getting the .hmm file
 docker run --rm --privileged -v $PWD/data:/data tm/hmmer hmmpress /data/Pfam-A.hmm
 
-docker run --rm --privileged -v $PWD/data:/data tm/hmmer hmmscan --tblout /data/hits.txt /data/Pfam-A.hmm /data/test_data.fasta
+docker run --rm --privileged -v $PWD/data-hmm:/data-hmm tm/hmmer hmmscan --tblout /data-hmm/results/hits.txt /data-hmm/Pfam-A.hmm /data-hmm/fasta/seq_diogo_03102016.fasta
--- developpement/dockers/hmmer/run_hmmsearch.sh
+++ developpement/dockers/hmmer/run_hmmsearch.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --rm --privileged -v ~/tm/hmmer/data:/data tm/hmmer hmmsearch --tblout /data/hits.txt /data/Pfam-A.hmm /data/test_data.fasta
+# hmmpress /data/Pfam-A.hmm only first time fter getting the .hmm file
+docker run --rm --privileged -v $PWD/data:/data tm/hmmer hmmpress /data/Pfam-A.hmm
+
+docker run --rm --privileged -v $PWD/data-hmm:/data-hmm tm/hmmer hmmsearch --tblout /data-hmm/results/hits.txt /data-hmm/Pfam-A.hmm /data-hmm/fasta/seq_diogo_03102016.fasta
