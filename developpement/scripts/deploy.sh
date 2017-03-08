b0ba6f846435edeebbaa2441f2a3a5205a9f3180
Déruaz Vincent <deruazvincent@gmail.com>
2017-03-08 11:14:31 +0100

    Update deploy.sh

--- developpement/scripts/deploy.sh
+++ developpement/scripts/deploy.sh
@@ -10,9 +10,9 @@ cd ../../database/data/
 wget https://www.dropbox.com/s/mzt9pxpfnvxl3wa/bacteriaVD.sql?dl=0
 mv bacteriaVD.sql?dl=0 bacteriaVD.sql
 
-cd ../../../inphinity/configs/v_0.3
-cp config.example.ini config_v0.3.ini
-sudo nano config_v0.3.ini
+cd ../../../inphinity/configs/v_0.5
+cp config.example.ini config_v0.5.ini
+sudo nano config_v0.5.ini
 
 cd ../../../compose/
 sudo sh run.sh
